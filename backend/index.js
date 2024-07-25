c3f1b2485c2fd2b1f040bd0ad0c64bc20c6892f4
Vaibhav Jain <88039285+VaibhavxLegend@users.noreply.github.com>
2024-07-25 23:34:30 +0530

    feat: new packages

--- backend/index.js
+++ backend/index.js
@@ -3,17 +3,25 @@ const app = express();
 
 const dishes=require("./routes/dishesRoutes");
 const registerUser=require("./routes/userRoutes");
+const mongoose=require('mongoose');
 
+mongoose.connect('mongodb://127.0.0.1:27017/Food_app')
+.then(()=> console.log("connected"))
 const port = 5000;
 
 app.use(express.json());
 
-app.use("/api",dishes);
 
+app.use((req,res,next)=>{
+  console.log("time :",Date.now());
+  next();
+})
+
+
+app.use("/api",dishes);
 app.use("/api", registerUser);
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
-});
-
-// 
+})  
